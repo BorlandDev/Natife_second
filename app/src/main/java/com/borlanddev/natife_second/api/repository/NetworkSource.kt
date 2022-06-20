d92928ccf5dd933a6da1a49ab3d99fca4fe4c910
BorlandDev <minsk1115596@gmail.com>
2022-06-20 15:41:28 +0300

    Bugfix

--- app/src/main/java/com/borlanddev/natife_second/api/repository/NetworkSource.kt
+++ app/src/main/java/com/borlanddev/natife_second/api/repository/NetworkSource.kt
@@ -1,11 +1,10 @@
 package com.borlanddev.natife_second.api.repository
 
-import com.borlanddev.natife_second.di.AppScope
 import com.borlanddev.natife_second.model.User
 
-@AppScope
 interface NetworkSource {
 
-    fun getUsers(pageIndex: Int, pageSize: Int,
+    fun getUsers(
+        pageIndex: Int, pageSize: Int,
     ): Result<List<User>>
 }
--- app/src/main/java/com/borlanddev/natife_second/database/LocalSource.kt
+++ app/src/main/java/com/borlanddev/natife_second/database/LocalSource.kt
@@ -1,9 +1,7 @@
 package com.borlanddev.natife_second.database
 
-import com.borlanddev.natife_second.di.AppScope
 import com.borlanddev.natife_second.model.UserDB
 
-@AppScope
 interface LocalSource {
 
     fun getUsersDB(limit: Int, offset: Int): List<UserDB>
