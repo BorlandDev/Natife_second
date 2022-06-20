a8fcea68c189e909ce7c1dadda38a02adc1d32b7
BorlandDev <minsk1115596@gmail.com>
2022-06-20 16:59:00 +0300

    Refactoring

--- app/src/main/java/com/borlanddev/natife_second/screens/details/DetailsFragment.kt
+++ app/src/main/java/com/borlanddev/natife_second/screens/details/DetailsFragment.kt
@@ -9,7 +9,7 @@ import com.borlanddev.natife_second.R
 import com.borlanddev.natife_second.base.BaseFragment
 import com.borlanddev.natife_second.databinding.DetailsFragnentBinding
 import com.bumptech.glide.Glide
-import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
 class DetailsFragment : BaseFragment<DetailsVM, DetailsFragnentBinding>() {
@@ -20,7 +20,7 @@ class DetailsFragment : BaseFragment<DetailsVM, DetailsFragnentBinding>() {
         }
 
     private val args: DetailsFragmentArgs by navArgs()
-    override val viewModel: DetailsVM by inject { parametersOf(args.id) }
+    override val viewModel: DetailsVM by viewModel { parametersOf(args.id) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
